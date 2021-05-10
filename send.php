519d4beaab1bf1ee4bab796387dea3f1c8e0de38
kiquino <kiquino@gmail.com>
2021-05-10 17:04:13 -0300

    mais php

---
+++ send.php
@@ -0,0 +1,16 @@
+<?php
+
+$nombre = $_POST['nombre'];
+
+$email = $_POST['mail'];
+
+$tel = $_POST['tel'];
+
+$mensaje = $_POST['text'];
+
+
+echo $nombre.' - '.$email.' - '.$tel.' - '.$text;
+
+
+
+?>
